f30bc532eb070c592646f74433c517922c760e69
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-05-26 15:12:39 -0500

    init (hello world de navigation) y estructura inicial de carpetas

---
+++ App.js
@@ -0,0 +1,75 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+function HomeScreen({navigation}) {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Button
+        title="Click"
+        onPress={() => navigation.navigate('Form', {foo: 'foofoo'})}
+      />
+    </View>
+  );
+}
+
+function FormScreen({navigation, route}) {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Button title="Foo" onPress={() => console.log(route.params.foo)} />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Form" component={FormScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
