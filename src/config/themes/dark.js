b42b22eccb07c395032d3d78b93efc22746441e6
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-05-26 17:16:17 -0500

    archivo con los valores que se reutilizaran, como los colores, tamaños de texto y padding y margins, esto con el fin de ser más semantico

---
+++ src/config/themes/dark.js
@@ -0,0 +1,38 @@
+const palette = {
+  orange: '#fa4d09',
+  white: '#ffffff',
+  blue: '#00609c',
+  gray: '#707070',
+  red: '#ff2626',
+  black: '#000000',
+  lightGray: '#afafaf',
+};
+
+const theme = {
+  color: {
+    p1: palette.orange,
+    p2: palette.white,
+    s1: palette.blue,
+    s2: palette.black,
+    link: palette.lightGray,
+    danger: palette.red,
+    other1: palette.gray,
+    other2: palette.lightGray,
+  },
+  font: {
+    s: 12,
+    m: 16,
+    l: 20,
+    xl: 24,
+    xxl: 28,
+  },
+  spacing: {
+    s: 5,
+    m: 10,
+    l: 15,
+    xl: 20,
+    xxl: 25,
+  },
+};
+
+export default theme;
