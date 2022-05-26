b1199b7cd9e23695b8e058923269c6866408fc48
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-05-26 17:22:18 -0500

    agregado el componente que se usara para la pantalla principal, aun no esta completado

---
+++ src/components/screens/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, ScrollView} from 'react-native';
+import Footer from '../../molecules/Footer';
+
+function Home() {
+  return (
+    <View>
+      <ScrollView>
+        <Footer />
+      </ScrollView>
+    </View>
+  );
+}
+
+export default Home;
