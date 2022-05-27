2bd80f9e33ff0d452b936a0e38d696c0639e3e3a
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-05-27 16:59:21 -0500

    implementación del progreso en el primer paso del formulario, faltan los campos y el submit

--- src/components/screens/FormStep1/index.js
+++ src/components/screens/FormStep1/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import {View, ScrollView} from 'react-native';
+import ThisIsAtomicLabs from '../../molecules/ThisIsAtomicLabs';
+import CustomImage from '../../atoms/CustomImage';
+import styles from './styles';
+import HomeBackgroundImage from '../../../images/MaskGroup1.png';
+import Footer from '../../molecules/Footer';
+import FormProgress from '../../molecules/FormProgress';
+import CustomText from '../../atoms/CustomText';
+import CustomSection from '../../layouts/CustomSection';
 
 function FormStep1() {
   return (
     <View>
       <ScrollView>
-        
+        <CustomSection>
+          <CustomImage
+            resizeMode="stretch"
+            style={styles.background1}
+            source={HomeBackgroundImage}
+          />
+          <ThisIsAtomicLabs />
+          <FormProgress step={2} />
+          <CustomText>
+            Queremos saber que eres tú, por favor ingresa los siguientes datos:
+          </CustomText>
+        </CustomSection>
+        <Footer />
       </ScrollView>
     </View>
   );
---
+++ src/components/screens/FormStep1/styles.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  background1: {
+    position: 'absolute',
+    left: -150,
+    top: -20,
+    width: '200%',
+    height: 500,
+  },
+});
+
+export default styles;
