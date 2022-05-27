ee50821b0e3b98f3942510a4345e39679b6dfe20
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-05-26 23:07:40 -0500

    implementadas nuevas secciones en la pantalla principal, ademas del uso de customimage

--- src/components/screens/Home/index.js
+++ src/components/screens/Home/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import {View, ScrollView} from 'react-native';
+import CustomImage from '../../atoms/CustomImage';
 import Footer from '../../molecules/Footer';
+import OurTeam from '../../organisms/OurTeam';
+import HomeBackgroundImage from '../../../images/MaskGroup1.png';
+import WorkingWithUs from '../../organisms/WorkingWithUs';
+import styles from './styles';
 
 function Home() {
   return (
     <View>
       <ScrollView>
+        <CustomImage style={styles.background} source={HomeBackgroundImage} />
+        <WorkingWithUs />
+        <OurTeam />
         <Footer />
       </ScrollView>
     </View>
