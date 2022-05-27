3c8f62653a01657689806b7f25273eae6637c5d8
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-05-26 22:54:31 -0500

    edite algunos de los valores y agregue nuevos, debido a que no puedo contemplar exactamente los valores que ocupare al inicio estare modificando este archivo

--- src/config/themes/dark.js
+++ src/config/themes/dark.js
@@ -14,7 +14,6 @@ const theme = {
     p2: palette.white,
     s1: palette.blue,
     s2: palette.black,
-    link: palette.lightGray,
     danger: palette.red,
     other1: palette.gray,
     other2: palette.lightGray,
@@ -22,9 +21,9 @@ const theme = {
   font: {
     s: 12,
     m: 16,
-    l: 20,
-    xl: 24,
-    xxl: 28,
+    l: 28,
+    xl: 36,
+    xxl: 42,
   },
   spacing: {
     s: 5,
@@ -32,6 +31,7 @@ const theme = {
     l: 15,
     xl: 20,
     xxl: 25,
+    ul: 50,
   },
 };
 
