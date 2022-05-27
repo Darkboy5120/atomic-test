00b705a35a97002a6519dc4ed0ecfec518db5309
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-05-27 14:44:08 -0500

    agregado resizeMode prop para ajustar los backgrounds de Home y de las demas pantallas

--- src/components/atoms/CustomImage/index.js
+++ src/components/atoms/CustomImage/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Image} from 'react-native';
 
-function CustomImage({source, width, height, size, style}) {
+function CustomImage({resizeMode, source, width, height, size, style}) {
   const defaultSize = 20;
   const sizeStyle = {
     width: width ?? size ?? defaultSize,
     height: height ?? size ?? defaultSize,
   };
   const imageStyle = [[sizeStyle, style]];
-  return <Image source={source} style={imageStyle} />;
+  return <Image resizeMode={resizeMode} source={source} style={imageStyle} />;
 }
 
 export default CustomImage;
