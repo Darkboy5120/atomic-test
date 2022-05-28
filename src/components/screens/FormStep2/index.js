87b075b62ff8dc5eb01007ea8a5609f6a0516b08
Hilarión <50677192+Darkboy5120@users.noreply.github.com>
2022-05-27 23:37:50 -0500

    implementado mock de api, el mock tiene un 50% de fallar y muestra un mensaje de error, al tener 3 errores o 1 exito avanza a la siguiente pantalla

--- src/components/screens/FormStep2/index.js
+++ src/components/screens/FormStep2/index.js
@@ -1,5 +1,5 @@
-import React, {useEffect} from 'react';
-import {View, ScrollView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, ScrollView, Alert} from 'react-native';
 import ThisIsAtomicLabs from '../../molecules/ThisIsAtomicLabs';
 import CustomImage from '../../atoms/CustomImage';
 import styles from './styles';
@@ -14,25 +14,50 @@ import useFormStep2 from '../../../CustomHooks/useFormStep2';
 import CustomButton from '../../atoms/CustomButton';
 import {useNavigation, useRoute} from '@react-navigation/native';
 
-function sendPhone(navigation, form, params) {
+function apiMock(firstname, lastname, phone) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('el api recibe la info');
+      const apiWentWrong = Math.floor(Math.random() * 2);
+      if (apiWentWrong === 0) {
+        reject({log: 'error interno foo foo', code: 500});
+      } else {
+        resolve({log: 'todo ok', code: 200});
+      }
+    }, 3000);
+  });
+}
+
+function sendPhone(navigation, form, params, increaseApiCount) {
   form.submit.setLoading(true);
-  setTimeout(() => {
-    console.log('firstname =>', params.firstname);
-    console.log('lastname =>', params.lastname);
-    console.log('phone =>', form.phone.value);
-    form.submit.setLoading(false);
-    navigation.navigate('FormSuccess');
-  }, 2000);
+  apiMock(params.firstname, params.lastname, form.phone.value)
+    .catch(e => {
+      increaseApiCount();
+      Alert.alert('Error', e.log);
+      return e;
+    })
+    .then(r => {
+      form.submit.setLoading(false);
+      if (r.code === 200) {
+        navigation.navigate('FormSuccess');
+      }
+    });
 }
 
 function Form() {
   const navigation = useNavigation();
   const route = useRoute();
   const form = useFormStep2();
+  const [useApiCount, setUseApiCount] = useState(0);
   useEffect(() => {
     const formIsOk = !form.phone.ok;
     form.submit.setOk(!formIsOk);
   }, [form]);
+  useEffect(() => {
+    if (useApiCount >= 3) {
+      navigation.navigate('FormSuccess');
+    }
+  }, [navigation, useApiCount]);
   return (
     <CustomFlex style={styles.formContainer}>
       <CustomInput label="Número de Celular" {...form.phone} />
@@ -41,7 +66,10 @@ function Form() {
         center
         title="Continuar"
         type="submit"
-        onPress={() => sendPhone(navigation, form, route.params)}
+        onPress={() => {
+          const increaseApiCount = () => setUseApiCount(useApiCount + 1);
+          sendPhone(navigation, form, route.params, increaseApiCount);
+        }}
       />
     </CustomFlex>
   );
